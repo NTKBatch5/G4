aaf4674cf1187c6a65da14fd85d06d8318e9673a
airmo <airmo@10.0.0.136>
2022-08-03 18:50:27 -0400

    update

--- src/team/g4/DemoG4.java
+++ src/team/g4/DemoG4.java
@@ -1,35 +1,34 @@
 package team.g4;
 
 public class DemoG4 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Test");
 //		System.out.println("Now this is my first test...");
-		
+
 		System.out.println(myName("Fiodor")); // you misses a closed parentheses
-		
+
 //		Can someone in the team fix this problem?
-		
+
 		System.out.println("Bashira......Test");
-		
+
 	}
 
-	public static String myName(String str){
-		
-		//test pull & push 
-		String name="Sonya";
+	public static String myName(String str) {
+
+		// test pull & push
+		String name = "Sonya";
 		System.out.println(name);
-		
-	
-		return str;	
-		
-	//Test cloning -- Fernando
+
+		return str;
+
+		// Test cloning -- Fernando
 	}
 	// Test (B)
-	
+
 	public static void lastName(String st) {
 		System.out.println("Poya");
-		
+
 	}
 	public static void firstName(String fname) {
 		System.out.println("Luis Miguel..");
